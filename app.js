a3767c3747bd5d5614110391a55bd0aa1ee76e2b
muminovbob <bobomurodm2004@gmail.com>
2024-03-11 10:46:07 +0500

    fundamental page is ready :)

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// local imports
+const indexController = require('./controllers/index'); 
+
+app.set('view engine', 'pug');
+
+// route specification
+app.use('/', indexController); 
+
+
+app.use('/css', express.static(__dirname + '/public/css')); 
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/images', express.static(__dirname + '/public/images'));
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
